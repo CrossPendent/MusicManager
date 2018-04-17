c9f6028a6242b0d0d70d24e3a8dca40fc9c713c7
CrossPendent <yaho818@gmail.com>
2018-04-17 15:57:18 +0900

    add sso login

--- MusicManagerPHP/index.php
+++ MusicManagerPHP/index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=MS949">
-<title>Insert title here</title>
-</head>
-<body>
-<p>This page uses frames. The current browser you are using does not support frames.</p>
 <?php
-    echo "hello, music manager php"
+include "components/header.php";
+include "components/main.php";
+include "components/footer.php";
 ?>
-</body>
-</html>
